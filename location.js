9e43445df5170b88010c1f00c5165dae18459f36
Ramadan <daudalaramadan2@gmail.com>
2024-03-23 14:28:37 +0000

    l

--- location.js
+++ location.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
-http.createServer((req, res) => {
-  console.log("koofi");
-});
+http
+  .createServer((req, res) => {
+    res.write("name");
+    res.end();
+  })
+  .listen(3000);
