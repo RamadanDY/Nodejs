37b78e17f87406d175c48ea8e156320a5f308593
Ramadan <daudalaramadan2@gmail.com>
2024-03-23 13:04:24 +0000

    creating a new file and writing smt into it

--- auth.js
+++ auth.js
@@ -1,2 +1,11 @@
 const username = "Ramadan";
 console.log(username);
+
+const fs = require("fs");
+fs.writeFile("newTextFile.txt", "Name:" + username, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("wrotefile");
+});
