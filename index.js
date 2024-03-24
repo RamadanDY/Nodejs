934a512bf37d03496e714929d2ce882f1a2a4cfe
Ramadan <daudalaramadan2@gmail.com>
2024-03-24 21:43:46 +0000

    middleware

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
 
-app
-  .get("/", (req, res) => {
-    res.send("hello world");
-  })
-  .listen(3000);
+app.get("/hello", function (req, res) {
+  res.send("Hello World!");
+});
+app.get("/hello", function (req, res) {
+  res.send("go gerri");
+});
+app.all("*", (req, res, next) => {
+  console.log("A request was received");
+  next(); // Pass control to the next middleware function/route handler
+});
+
+app.listen(8000);
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/app", (req, res) => {
+//   res.send("hello world");
+// });
+// app.listen(3000);
