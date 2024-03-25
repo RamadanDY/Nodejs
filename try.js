4d2cb00c1472b3cf5aea744c089d01f89cf1fd62
Ramadan <daudalaramadan2@gmail.com>
2024-03-25 23:39:23 +0000

    creating a new file using the writefile module

--- try.js
+++ try.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const app = express();
-app.
+const fs = require("fs");
+
+const username = "ramadan";
+
+fs.writeFile("thenewface.txt", "Ramadan:" + username, function (req, res) {
+  if (err) {
+    console.log("err");
+  } else {
+    console.log("wrote file");
+  }
+});
