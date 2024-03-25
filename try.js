9fde41225016f59a92193a7e06f28330216f1a8a
Ramadan <daudalaramadan2@gmail.com>
2024-03-25 23:55:51 +0000

    dont

--- try.js
+++ try.js
@@ -1,14 +1,24 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
+var http = require("http");
 
-const username = "ramadan";
+http
+  .createServer(function (req, res) {
+    // res.writeHead(200, { "content-Type": "text/html" });
+    res.write(req.url);
+    res.end("<h1>the name of the school is </h1>");
+  })
+  .listen(8000);
 
-fs.writeFile("thenewface.txt", "Ramadan:" + username, function (req, res) {
-  if (err) {
-    console.log("err");
-    return;
-  } else {
-    console.log("wrote file");
-  }
-});
+// const express = require("express");
+// const app = express();
+// const fs = require("fs");
+
+// const username = "ramadan";
+
+// fs.writeFile("thenewface.txt", "Ramadan:" + username, function (req, res) {
+//   if (err) {
+//     console.log("err");
+//     return;
+//   } else {
+//     console.log("wrote file");
+//   }
+// });
